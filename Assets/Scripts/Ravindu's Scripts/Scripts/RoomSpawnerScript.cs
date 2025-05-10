6a242172c30cce2bb3c7ce802fe7aa97621ab8da
ravin <hewabadd@sheridancollege.ca>
2025-05-10 15:54:43 -0400

    created wall placement coordinates now importing daniel's wall prefab and using that for the rest of the code and possibly altering the room coordinates

--- Assets/Scripts/Ravindu's Scripts/Scripts/RoomSpawnerScript.cs
+++ Assets/Scripts/Ravindu's Scripts/Scripts/RoomSpawnerScript.cs
@@ -8,6 +8,7 @@ public class RoomSpawnerScript : MonoBehaviour
 {
     public List<GameObject> roomList = new List<GameObject>();
     private List<Vector3> roomCoordinatesList = new List<Vector3>();
+    private List<Vector3> wallCoordinatesList = new List<Vector3>();
     public List<Material> materialList = new List<Material>();
     public GameObject wallPrefab;
     public AudioClip fallingsfx;
@@ -19,19 +20,7 @@ public class RoomSpawnerScript : MonoBehaviour
     void Start()
     {
         audiosource = gameObject.GetComponent<AudioSource>();
-
-
-        roomCoordinatesList.Add(new Vector3(10, -0.5f, 10));
-        roomCoordinatesList.Add(new Vector3(0, -0.5f, 0));
-        roomCoordinatesList.Add(new Vector3(0, -0.5f, 10));
-        roomCoordinatesList.Add(new Vector3(0, -0.5f, 20));
-        roomCoordinatesList.Add(new Vector3(10, -0.5f, 20));
-        roomCoordinatesList.Add(new Vector3(20, -0.5f, 20));
-        roomCoordinatesList.Add(new Vector3(20, -0.5f, 10));
-        roomCoordinatesList.Add(new Vector3(20, -0.5f, 0));
-        roomCoordinatesList.Add(new Vector3(10, -0.5f, 0));
-
-
+        createRoomCoordList();
         while (usedRoomsList.Count < 8)
         {
             int rand = Random.Range(1, 9);
@@ -53,12 +42,12 @@ public class RoomSpawnerScript : MonoBehaviour
     {
         SpawnCenterBlock();
         yield return new WaitForSeconds(1f);
-        int count=1;
-        while (count <= 9)
+        int count = 1;
+        while (count < 9)
         {
-            for(count =1; count<9; count++)
+            for (count = 1; count < 9; count++)
             {
-                GameObject room = Instantiate(roomList[usedRoomsList[count]]);
+                GameObject room = Instantiate(roomList[usedRoomsList[count - 1]]);
                 Vector3 upperPosition = roomCoordinatesList[count];
                 upperPosition.y = 50;
                 room.transform.position = upperPosition;
@@ -67,16 +56,16 @@ public class RoomSpawnerScript : MonoBehaviour
 
             }
         }
+        StartCoroutine(WallSpawner());
     }
     public IEnumerator RoomSpawning(GameObject spawnedRoom, Vector3 endPos)
     {
-        
+
         Vector3 StartPos = spawnedRoom.transform.position;
         float t = 0;
         while (t < 1)
         {
             t += Time.deltaTime * 0.5f;
-            print(t);
             spawnedRoom.transform.position = Vector3.Lerp(StartPos, endPos, fallingAnimation.Evaluate(t));
             if (t > 1)
             {
@@ -85,6 +74,21 @@ public class RoomSpawnerScript : MonoBehaviour
             yield return null;
         }
 
+    }
+
+    public IEnumerator WallSpawner()
+    {
+        int i = 0;
+        while (i < 24)
+        {
+            foreach(Vector3 j in roomCoordinatesList)
+            {
+                i++;
+                GameObject spawnedWall = Instantiate(wallPrefab);
+                WallScript wallScript = spawnedWall.GetComponent<WallScript>();
+            }
+        }
+        yield return null;
     }
 
 
@@ -130,5 +134,49 @@ public class RoomSpawnerScript : MonoBehaviour
     private float sineAmount(float t)
     {
         return Mathf.Sin(t * 6 / 0.15f) * 0.2f;
+    }
+
+    private void createRoomCoordList()
+    {
+        roomCoordinatesList.Add(new Vector3(10, -0.5f, 10));
+        roomCoordinatesList.Add(new Vector3(0, -0.5f, 0));
+        roomCoordinatesList.Add(new Vector3(0, -0.5f, 10));
+        roomCoordinatesList.Add(new Vector3(0, -0.5f, 20));
+        roomCoordinatesList.Add(new Vector3(10, -0.5f, 20));
+        roomCoordinatesList.Add(new Vector3(20, -0.5f, 20));
+        roomCoordinatesList.Add(new Vector3(20, -0.5f, 10));
+        roomCoordinatesList.Add(new Vector3(20, -0.5f, 0));
+        roomCoordinatesList.Add(new Vector3(10, -0.5f, 0));
+    }
+
+    private void createWallCoordList()
+    {
+        wallCoordinatesList.Add(new Vector3(4.82999992f, 1.5854841e-05f, 12.0200005f));
+        wallCoordinatesList.Add(new Vector3(11.969986f, 1.58548355e-05f, 15.140192f));
+        wallCoordinatesList.Add(new Vector3(14.8299122f, 1.58548355e-05f, 11.9999943f));
+        wallCoordinatesList.Add(new Vector3(11.969986f, 1.58548355e-05f, 5.14019203f));
+        wallCoordinatesList.Add(new Vector3(-1.99999523f, 1.58548355e-05f, 4.85991859f));
+        wallCoordinatesList.Add(new Vector3(-5.14008617f, 1.58548355e-05f, 11.9999943f));
+        wallCoordinatesList.Add(new Vector3(-5.14008522f, 1.58548355e-05f, 21.9999924f));
+        wallCoordinatesList.Add(new Vector3(-1.99999523f, 1.58548355e-05f, 24.8599205f));
+        wallCoordinatesList.Add(new Vector3(8.00000763f, 1.58548355e-05f, 24.8599205f));
+        wallCoordinatesList.Add(new Vector3(18.0000038f, 1.58548355e-05f, 24.8599205f));
+        wallCoordinatesList.Add(new Vector3(24.8599129f, 1.58548355e-05f, 21.9999924f));
+        wallCoordinatesList.Add(new Vector3(24.8599129f, 1.58548355e-05f, 11.9999943f));
+        wallCoordinatesList.Add(new Vector3(24.8599129f, 1.58548355e-05f, 11.9999943f));
+        wallCoordinatesList.Add(new Vector3(18.0000038f, 1.58548355e-05f, -5.14008045f));
+        wallCoordinatesList.Add(new Vector3(8.00000572f, 1.58548355e-05f, -5.14008045f));
+        wallCoordinatesList.Add(new Vector3(-1.99999523f, 1.58548355e-05f, -5.14008045f));
+        wallCoordinatesList.Add(new Vector3(-5.14008522f, 1.58548355e-05f, 1.99999237f));
+        wallCoordinatesList.Add(new Vector3(-1.99999523f, 1.58548355e-05f, 14.8599205f));
+        wallCoordinatesList.Add(new Vector3(4.85991287f, 1.58548355e-05f, 21.9999924f));
+        wallCoordinatesList.Add(new Vector3(14.8599138f, 1.58548355e-05f, 21.9999943f));
+        wallCoordinatesList.Add(new Vector3(18.0000038f, 1.58548355e-05f, 14.8599195f));
+        wallCoordinatesList.Add(new Vector3(18.0000038f, 1.58548355e-05f, 4.85991859f));
+        wallCoordinatesList.Add(new Vector3(14.8599138f, 1.58548355e-05f, 1.99999237f));
+        wallCoordinatesList.Add(new Vector3(4.85991287f, 1.58548355e-05f, 1.99999237f));
+
+
+
     }
 }
--- Assets/Scripts/Ravindu's Scripts/Scripts/WallScript.cs
+++ Assets/Scripts/Ravindu's Scripts/Scripts/WallScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class WallScript : MonoBehaviour
 {
+    public GameObject firstWall;
+    public GameObject secondWall;
+    public GameObject DoorLeft;
+    public GameObject DoorRight;
     // Start is called before the first frame update
     void Start()
     {
