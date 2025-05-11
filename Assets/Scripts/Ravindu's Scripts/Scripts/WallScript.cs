9315d6df42c6c6c2dfe8bebfe244d171991f8ec7
ravin <hewabadd@sheridancollege.ca>
2025-05-10 22:28:18 -0400

    pre-spawned walls at top and doing mat 1 mat 2 to select wallpapers

--- Assets/Scripts/Ravindu's Scripts/Scripts/WallScript.cs
+++ Assets/Scripts/Ravindu's Scripts/Scripts/WallScript.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class WallScript : MonoBehaviour
 {
+    public RoomSpawnerScript roomSpawner;
     public GameObject firstWall;
     public GameObject secondWall;
     public GameObject DoorLeft;
     public GameObject DoorRight;
+    public AnimationCurve fallingCurve;
+    public int mat1;
+    public int mat2;
+    public MeshRenderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(falling() );
+        meshRenderer = GetComponent<MeshRenderer>();
+        ChooseMat();
     }
 
     // Update is called once per frame
@@ -20,8 +27,28 @@ public class WallScript : MonoBehaviour
         
     }
 
-    public void rotateWallRightAngle()
+    public IEnumerator falling()
+    {
+        float t = 0;
+        Vector3 startPos = transform.position;
+        Vector3 endPos = startPos;
+        endPos.y = 1.58548355e-05f;
+        while (t < 1)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, fallingCurve.Evaluate(t));
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void ChooseMat()
+    {
+        meshRenderer.materials.SetValue(roomSpawner.materialList[mat1], 1);
+        meshRenderer.materials.SetValue(roomSpawner.materialList[mat2], 2);
+    }
+
+    /*public void rotateWallRightAngle()
     {
         transform.Rotate(0, 90, 0);
-    }
+    }*/
 }
