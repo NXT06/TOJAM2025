cd4fdaf7ead6220b56a5d83ca2454ee7114f6f27
Alpkurt2 <alpkurt22@hotmail.com>
2025-05-10 22:06:12 -0400

    Implemented Lives

--- Assets/Scripts/Alp's Scripts/Lives.cs
+++ Assets/Scripts/Alp's Scripts/Lives.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lives : MonoBehaviour
 {
+    public int lives = 3;
+    public Sprite sprite;
+    public Image life1, life2, life3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //life1 = GetComponent<Image>();
+        //life2 = GetComponent<Image>();
+        //life3 = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (lives == 3)
+        {
+            life1.color = new Color(255, 255, 255, 255);
+            life2.color = new Color(255, 255, 255, 255);
+            life3.color = new Color(255, 255, 255, 255);
+        }
+        if (lives == 2)
+        {
+            life1.color = new Color(255, 255, 255, 255);
+            life2.color = new Color(255, 255, 255, 255);
+            life3.color = new Color(255, 255, 255, 0);
+        }
+        if (lives == 1)
+        {
+            life1.color = new Color(255, 255, 255, 255);
+            life2.color = new Color(255, 255, 255, 0);
+            life3.color = new Color(255, 255, 255, 0);
+        }
+        if (lives == 0)
+        {
+            life1.color = new Color(255, 255, 255, 0);
+            life2.color = new Color(255, 255, 255, 0);
+            life3.color = new Color(255, 255, 255, 0);
+        }
     }
+
+
 }
